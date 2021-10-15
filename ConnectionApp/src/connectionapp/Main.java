e450d6f00a6ac71075a67b2c9adc137f31955231
eliasgz97 <47019721+eliasgz97@users.noreply.github.com>
2021-10-15 14:20:35 -0600

    Lectura de archivos de excel completada

--- ConnectionApp/src/connectionapp/Main.java
+++ ConnectionApp/src/connectionapp/Main.java
@@ -8,29 +8,16 @@ package connectionapp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
+//import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
-/*import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-*/
 /**
  *
  * @author Daniel RC
@@ -55,11 +42,11 @@ public class Main extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-        jb_actualizar = new javax.swing.JButton();
+        jbt_actualizar = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
-        jb_salir = new javax.swing.JButton();
+        jbt_verReportes = new javax.swing.JButton();
+        jbt_salir = new javax.swing.JButton();
 
         jLabel1.setText("jLabel1");
 
@@ -68,11 +55,11 @@ public class Main extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(51, 102, 0));
 
-        jb_actualizar.setText("Actualizar Listas");
-        jb_actualizar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0)));
-        jb_actualizar.addActionListener(new java.awt.event.ActionListener() {
+        jbt_actualizar.setText("Actualizar Listas");
+        jbt_actualizar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0)));
+        jbt_actualizar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jb_actualizarActionPerformed(evt);
+                jbt_actualizarActionPerformed(evt);
             }
         });
 
@@ -83,19 +70,19 @@ public class Main extends javax.swing.JFrame {
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Connection App");
 
-        jButton2.setText("Ver reportes");
-        jButton2.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0)));
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        jbt_verReportes.setText("Ver reportes");
+        jbt_verReportes.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0)));
+        jbt_verReportes.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jbt_verReportesActionPerformed(evt);
             }
         });
 
-        jb_salir.setText("Salir");
-        jb_salir.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0)));
-        jb_salir.addActionListener(new java.awt.event.ActionListener() {
+        jbt_salir.setText("Salir");
+        jbt_salir.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0), new java.awt.Color(153, 153, 0)));
+        jbt_salir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jb_salirActionPerformed(evt);
+                jbt_salirActionPerformed(evt);
             }
         });
 
@@ -105,7 +92,7 @@ public class Main extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jb_salir, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jbt_salir, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(34, 34, 34))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -119,10 +106,10 @@ public class Main extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(238, 238, 238)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jb_actualizar, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jbt_actualizar, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(15, 15, 15)
-                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(jbt_verReportes, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(201, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -133,11 +120,11 @@ public class Main extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 146, Short.MAX_VALUE)
-                .addComponent(jb_actualizar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jbt_actualizar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(73, 73, 73)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(143, 143, 143)
-                .addComponent(jb_salir, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jbt_verReportes, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(148, 148, 148)
+                .addComponent(jbt_salir)
                 .addGap(32, 32, 32))
         );
 
@@ -158,45 +145,45 @@ public class Main extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jb_actualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jb_actualizarActionPerformed
-      try {             String name="m";
-			FileInputStream file = new FileInputStream(new File(name));
-			Workbook workbook = new XSSFWorkbook(file);
-			DataFormatter dataFormatter = new DataFormatter();
-			Iterator<Sheet> sheets = workbook.sheetIterator();
-			while(sheets.hasNext()) {
-				Sheet sh = sheets.next();
-				System.out.println("Sheet name is "+sh.getSheetName());
-				System.out.println("---------");
-				Iterator<Row> iterator = sh.iterator();
-				while(iterator.hasNext()) {
-					Row row = iterator.next();
-					Iterator<Cell> cellIterator = row.iterator();
-					while (cellIterator.hasNext()) {
-						Cell cell = cellIterator.next();
-						String cellValue = dataFormatter.formatCellValue(cell);
-						//if(cell.getCellType() == CellType.STRING) {
-						//	
-						//}
-						System.out.print(cellValue+"\t");
-					}
-					System.out.println();
-				}
-			}
-			workbook.close();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-    }//GEN-LAST:event_jb_actualizarActionPerformed
+    private void jbt_actualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbt_actualizarActionPerformed
+        try {
+            String name = "C:/Users/EliasGZ/Documents/Software_HumuyaInn/EmailCampaignConnection/ConnectionApp/src/FormatoSalidaLista.xlsx";
+            FileInputStream file = new FileInputStream(new File(name));
+            Workbook workbook = new XSSFWorkbook(file);
+            DataFormatter dataFormatter = new DataFormatter();
+            Iterator<Sheet> sheets = workbook.sheetIterator();
+            while (sheets.hasNext()) {
+                Sheet sh = sheets.next();
+                System.out.println("Sheet name is " + sh.getSheetName());
+                System.out.println("---------");
+                Iterator<Row> iterator = sh.iterator();
+                while (iterator.hasNext()) {
+                    Row row = iterator.next();
+                    Iterator<Cell> cellIterator = row.iterator();
+                    while (cellIterator.hasNext()) {
+                        Cell cell = cellIterator.next();
+                        String cellValue = dataFormatter.formatCellValue(cell);
+                        //if(cell.getCellType() == CellType.STRING) {
+                        //	
+                        //}
+                        System.out.print(cellValue + "\t");
+                    }
+                    System.out.println();
+                }
+            }
+            workbook.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_jbt_actualizarActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-      
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void jbt_verReportesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbt_verReportesActionPerformed
 
-    private void jb_salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jb_salirActionPerformed
-      
-    }//GEN-LAST:event_jb_salirActionPerformed
+    }//GEN-LAST:event_jbt_verReportesActionPerformed
+
+    private void jbt_salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbt_salirActionPerformed
+
+    }//GEN-LAST:event_jbt_salirActionPerformed
 
     /**
      * @param args the command line arguments
@@ -212,28 +199,24 @@ public class Main extends javax.swing.JFrame {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
-                
 
-}
+                }
             }
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(Main.class
-.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
 
-} catch (InstantiationException ex) {
+        } catch (InstantiationException ex) {
             java.util.logging.Logger.getLogger(Main.class
-.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
 
-} catch (IllegalAccessException ex) {
+        } catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(Main.class
-.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
 
-} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Main.class
-.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -246,12 +229,12 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JButton jb_actualizar;
-    private javax.swing.JButton jb_salir;
+    private javax.swing.JButton jbt_actualizar;
+    private javax.swing.JButton jbt_salir;
+    private javax.swing.JButton jbt_verReportes;
     // End of variables declaration//GEN-END:variables
 }
